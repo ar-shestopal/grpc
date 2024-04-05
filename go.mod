8aef0303317ce312f47f413d8db2f21d012c15d8
oshe <oleksandr.shestopal@outreach.io>
2024-04-05 13:12:15 +0300

    Main structure added

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/ar-shestopal/grpc-events
-
-go 1.21.5
---
+++ sso/cmd/main.go
@@ -0,0 +1,11 @@
+package sso
+
+func main() {
+	// TODO initiallize config
+
+	// TODO initialize logger
+
+	// TODO initialize app (app)
+
+	// TODO initialize grpc server
+}
